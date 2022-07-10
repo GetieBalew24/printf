b2a86fca46addb7b923d8fab4df94e95437c174d
Buka Pitch <70853164+CreativeTechET@users.noreply.github.com>
2022-07-10 13:41:08 +0300

    Test

---
+++ Test/main.h
@@ -0,0 +1,31 @@
+#ifndef _MAIN_
+#define _MAIN_
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+
+
+/**
+ * struct format - match the conversion specifiers
+ * @id: type char pointer
+ * @f: type pointer to function
+ */
+
+typedef struct format
+{
+	char *id;
+	int (*f)();
+} convert_match;
+
+
+int printf_string(va_list val);
+int printf_char(va_list val);
+int printf_37(void);
+int _strlen(char *str);
+int _strlenc(const char *str);
+
+#endif
