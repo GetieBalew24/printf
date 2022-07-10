cf13cc64d41abd2009e31dd15f74535c84889ab0
Buka Pitch <70853164+CreativeTechET@users.noreply.github.com>
2022-07-10 12:35:08 +0300

    main header

--- main.h
+++ main.h
@@ -2,8 +2,22 @@
 #define _MAIN_
 
 #include <stdio.h>
+#include <stdarg.h>
 
 int _printf(const char *format, ...);
 int _putchar(char c);
+
+
+/**
+ * struct format - match the conversion specifiers
+ * @id: type char pointer
+ * @f: type pointer to function
+ */
+
+typedef struct format
+{
+	char *id;
+	int (*f)();
+} convert_match;
 
 #endif
