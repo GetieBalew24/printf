cf4d45417a6f4888a95512c26ff0066ff0392e3f
GetieBalew24 <getiebalew2020@gmail.com>
2022-07-13 08:53:30 -0700

    Header file

--- main.h
+++ main.h
@@ -32,5 +32,7 @@ int printf_int(va_list arg);
 int printf_bin(va_list val);
 int print_unsigned(va_list u);
 int print_octal(va_list o);
-int print_hex(unsigned int n, unsigned int c); 
+int print_hex(unsigned int n, unsigned int c);
+int print_x(va_list x);
+int print_X(va_list X);
 #endif
